26d6c6895c963782eaf4416583a3c934e9791ecf
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2022-04-07 21:41:26 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Акбала Тлеугалиева on 4/7/22.
+//  Copyright © 2022 Akbala Tleugaliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
